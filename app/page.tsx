7c45a7d38cb00662aca797d07199c9356b620c6f
Mathys Basson <pieterm.basson@gmail.com>
2025-11-07 22:01:11 +0200

    Styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,28 +3,18 @@ import gsapLogo from "@/assets/logos/gsap.svg";
 import motionLogo from "@/assets/logos/motion.svg";
 
 export default function Home() {
-    return (
-        <div className="flex min-h-screen items-center justify-center gap-20 bg-zinc-200 font-sans">
-            <a href="/motion">
-                <div className="home-page-card">
-                    <Image
-                        src={motionLogo}
-                        width={100}
-                        height={100}
-                        alt="Motion Logo"
-                    />
-                </div>
-            </a>
-            <a href="/gsap">
-                <div className="home-page-card">
-                    <Image
-                        src={gsapLogo}
-                        width={100}
-                        height={100}
-                        alt="GSAP Logo"
-                    />
-                </div>
-            </a>
+  return (
+    <div className="flex min-h-screen items-center justify-center gap-20 bg-zinc-200 font-sans">
+      <a href="/motion">
+        <div className="home-page-card hover:bg-yellow-300">
+          <Image src={motionLogo} width={100} height={100} alt="Motion Logo" />
         </div>
-    );
+      </a>
+      <a href="/gsap">
+        <div className="home-page-card hover:bg-green-500">
+          <Image src={gsapLogo} width={100} height={100} alt="GSAP Logo" />
+        </div>
+      </a>
+    </div>
+  );
 }
